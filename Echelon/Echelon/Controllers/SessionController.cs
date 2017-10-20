bf1c0c0ce4042460e7c94c2b3f9e99d17114873e
joshpadams123 <31546513+joshpadams123@users.noreply.github.com>
2017-10-20 11:33:20 -0400

    Justin Is Stupid

--- Echelon/Echelon/Controllers/SessionController.cs
+++ Echelon/Echelon/Controllers/SessionController.cs
@@ -12,5 +12,10 @@ namespace Echelon.Controllers
         {
             return View();
         }
+
+        public IActionResult JustinIsStupid()
+        {
+            return View();
+        }
     }
 }
