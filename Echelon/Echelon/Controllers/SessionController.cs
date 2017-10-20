82b1f82b8fa4ad87162d2a70022dbf1b30aea8d1
KeiroAkiza <31546511+KeiroAkiza@users.noreply.github.com>
2017-10-20 11:26:46 -0400

    Added Session controller and home page.

---
+++ Echelon/Echelon/Controllers/SessionController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Echelon.Controllers
+{
+    public class SessionController : Controller
+    {
+        public IActionResult Home()
+        {
+            return View();
+        }
+    }
+}
